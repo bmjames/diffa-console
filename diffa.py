e90f724257a65526a7ada46a682e9de07242f2b8
Ben James <bmjames@gmail.com>
2011-07-23 23:30:59 +0100

    Put time labels to the right of the time they indicate

--- diffa.py
+++ diffa.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 import json
 import datetime
+import logging
 from restful_lib import Connection
+import sys
 from urlparse import urljoin
 
 DATETIME_FORMAT = '%Y%m%dT%H%M%SZ'
@@ -15,17 +17,24 @@ DEFAULT_WIDTH = 80
 LABEL_WIDTH = 10
 
 class DiffsClient(object):
-    def __init__(self, agent_url):
+
+    _logger = logging.getLogger('DiffsClient')
+    _logger.addHandler(logging.StreamHandler(sys.stderr))
+
+    def __init__(self, agent_url, verbose=False):
+        self._logger.setLevel(logging.DEBUG if verbose else logging.NOTSET)
         if agent_url[-1] != "/":
             agent_url += "/"
         self.agent_url = agent_url
         base_url = urljoin(agent_url, 'rest')
         self.conn = Connection(base_url)
-        self.session_url = self.get_session_url()
-        self.conn = Connection(self.session_url)
+        self.conn = Connection(self.get_session_url())
 
     def get_session_url(self):
-        response = self.conn.request_post('/diffs/sessions')
+        url = '/diffs/sessions'
+        self._logger.debug("Getting session URL from %s", self._rebuild_url(url))
+        response = self.conn.request_post(url)
+        self._logger.debug("Got response: %s", response)
         return response['headers']['location']
 
     def get_diffs(self, range_start, range_end, bucketing):
@@ -34,8 +43,13 @@ class DiffsClient(object):
                 range_start.strftime(DATETIME_FORMAT),
                 range_end.strftime(DATETIME_FORMAT),
                 bucketing)
+        self._logger.debug("Getting diffs from %s", self._rebuild_url(url))
         response = self.conn.request_get(url)
+        self._logger.debug("Got response: %s", response)
         return json.loads(response['body'])
+
+    def _rebuild_url(self, url):
+        return self.conn.url.geturl() + url
 
     def __repr__(self):
         return "DiffsClient(%s)" % repr(self.agent_url)
@@ -44,7 +58,7 @@ def format_y_label(pair_key, width=LABEL_WIDTH):
     return pair_key[0:width-1].ljust(width)
 
 def format_x_label(time, width=LABEL_WIDTH):
-    return time.strftime("%H:%M >")[0:width].rjust(width)
+    return time.strftime("|%H:%M")[0:width].ljust(width)
 
 def round_down_to_hour(date_time):
     return date_time.replace(minute=0, second=0)
@@ -76,9 +90,9 @@ def print_time_axis(width, start_time, end_time):
     heatmap_width = width - LABEL_WIDTH # allow for y-axis labels
     timespan = end_time - start_time
     time_per_char = timespan / heatmap_width
-    line = format_x_label(start_time)
+    line = " " * LABEL_WIDTH
     for i in xrange(heatmap_width / LABEL_WIDTH):
-        time = start_time + (i + 1) * (time_per_char * LABEL_WIDTH)
+        time = start_time + i * (time_per_char * LABEL_WIDTH)
         line += format_x_label(time)
     print line
 
@@ -105,11 +119,12 @@ def main():
             help='show diffs until this UTC time (default: now)')
 
     add_arg('-w', dest='width', metavar='WIDTH', type=int, default=DEFAULT_WIDTH,
-            help='character width of output (default: 80)')
+            help='width (in characters) of output (default: 80)')
+    add_arg('-v', dest='verbose', action='store_true', help='show HTTP activity')
 
     args = parser.parse_args()
 
-    client = DiffsClient(args.agent_url)
+    client = DiffsClient(args.agent_url, args.verbose)
     
     start_time = datetime.datetime.strptime(args.start_time, DATETIME_FORMAT)
     end_time = datetime.datetime.strptime(args.end_time, DATETIME_FORMAT)
